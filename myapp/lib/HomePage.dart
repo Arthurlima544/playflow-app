ef5a8ef6b8674ae646184f4be54cf02a60db45de
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-01-29 16:58:36 -0300

    Adding initial page

---
+++ myapp/lib/HomePage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const SafeArea(
+          child: Center(
+        child: Text("Olá !!"),
+      )),
+    );
+  }
+}
