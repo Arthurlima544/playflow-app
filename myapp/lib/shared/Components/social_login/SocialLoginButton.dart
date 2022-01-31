e3ca4bd148ceca82cc9240f543e806caf6a88233
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-01-31 19:56:44 -0300

    Criando botão personalizado para login Google
    Inkwell -> permite callbacks como onTap em qualquer lugar da widget
    Para personalizar a posição das widgets dentro de uma Row precisamos colocar Expanded nos seus filhos e alterar a propriedade flex

---
+++ myapp/lib/shared/Components/social_login/SocialLoginButton.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/shared/theme/AppColors.dart';
+import 'package:myapp/shared/theme/AppImages.dart';
+import 'package:myapp/shared/theme/AppTextStyles.dart';
+
+class SocialLoginButton extends StatelessWidget {
+  final VoidCallback onTap;
+  const SocialLoginButton({
+    Key? key,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 56,
+        decoration: BoxDecoration(
+          color: AppColors.shape,
+          borderRadius: BorderRadius.circular(5),
+          border: Border.fromBorderSide(BorderSide(color: AppColors.stroke)),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+                flex: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(AppImages.google),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Container(
+                      height: 56,
+                      width: 1,
+                      color: AppColors.stroke,
+                    )
+                  ],
+                )),
+            Expanded(
+              flex: 4,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Entrar com Google",
+                    style: TextStyles.buttonGray,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
