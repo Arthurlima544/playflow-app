19ca92fc3786ce69559c243e163c970be1373f1b
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-01-31 16:10:24 -0300

    Refatoração da LoginPage: "final size = MediaQuery.of(context).size;" -> para armazenar o tamanho total da tela Container tamanho size -> para trabalhar com todo espaço disponivel Stack para sobrepor widgets Positioned para posicionar widgets em pontos especificos da tela

--- myapp/lib/Pages/LoginPage.dart
+++ myapp/lib/Pages/LoginPage.dart
@@ -1,102 +1,70 @@
 import 'package:flutter/material.dart';
-
-import 'components/AppBarContainer.dart';
+import 'package:myapp/shared/Components/social_login/SocialLoginButton.dart';
+import 'package:myapp/shared/theme/AppColors.dart';
+import 'package:myapp/shared/theme/AppImages.dart';
+import 'package:myapp/shared/theme/AppTextStyles.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Theme.of(context).backgroundColor,
-      child: SafeArea(
-        child: Column(
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Container(
+        height: size.height,
+        width: size.width,
+        child: Stack(
           children: [
-            Stack(
-              children: [
-                AppBarContainer(
-                  progress: 0.5,
-                  height: 600,
-                  width: MediaQuery.of(context).size.width,
-                  backgroundColor:
-                      Theme.of(context).appBarTheme.backgroundColor,
-                  progressColor: Theme.of(context).backgroundColor,
-                ),
-                const Positioned(
-                  child: Image(image: AssetImage("assets/woman.png")),
-                  bottom: 189,
-                  left: 118.85,
-                ),
-                Positioned(
-                  width: MediaQuery.of(context).size.width / 1.5,
-                  bottom: 50,
-                  left: 65.45,
-                  child: Text(
-                    "Organize seus boletos em um só lugar",
-                    style: Theme.of(context).textTheme.headline4,
-                    overflow: TextOverflow.visible,
-                  ),
-                ),
-              ],
+            Container(
+              width: size.width,
+              height: size.height * 0.36,
+              color: AppColors.primary,
+            ),
+            // TODO: Porque a imagem está fixa em um só ponto?
+            Positioned(
+              top: 40,
+              left: 0,
+              right: 0,
+              child: Image.asset(
+                AppImages.person,
+                width: 208,
+                height: 373,
+              ),
             ),
             Positioned(
-              bottom: 0,
-              left: 65.45,
-              child: SizedBox(
-                height: 80,
-                width: 300,
-                child: TextButton(
-                  style: ButtonStyle(
-                    elevation: MaterialStateProperty.all(3),
-                    backgroundColor:
-                        MaterialStateProperty.all(Colors.grey[200]),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5.0),
-                      ),
+              bottom: size.height * 0.1,
+              right: 0,
+              left: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(AppImages.logomini),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 70, right: 70, top: 30),
+                    child: Text(
+                      "Organize seus boletos em um só lugar",
+                      textAlign: TextAlign.center,
+                      style: TextStyles.titleHome,
                     ),
                   ),
-                  onPressed: () {
-                    print("OIIIII");
-                  },
-                  child: Row(
-                    children: [
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Align(
-                          alignment: AlignmentDirectional.center,
-                          child: Container(
-                            width: 30,
-                            height: 30,
-                            child: Image(
-                                image: AssetImage('assets/google_icon.png')),
-                          )),
-                      const SizedBox(
-                        width: 50,
-                      ),
-                      Positioned(
-                          right: 30,
-                          child: Text(
-                            "Entrar com Google",
-                            style: Theme.of(context).textTheme.headline6,
-                          ))
-                    ],
+                  Padding(
+                    padding: EdgeInsets.only(left: 40, right: 40, top: 40),
+                    child: SocialLoginButton(
+                      onTap: () {
+                        print("object");
+                      },
+                    ),
                   ),
-                ),
+                ],
               ),
-            )
+            ),
           ],
         ),
       ),
     );
   }
 }
-/* 
-style: TextButton.styleFrom(
-                    padding: const EdgeInsets.all(16.0),
-                    primary: Colors.white,
-                    textStyle: const TextStyle(fontSize: 20),
-                  ),
-
-*/
