5ef7d7684bb237cc020d1fc4ff38fe0a9c0641a0
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-01-31 19:56:04 -0300

    adicionando Splash page

---
+++ myapp/lib/Pages/SplashPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/shared/theme/AppColors.dart';
+import 'package:myapp/shared/theme/AppImages.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Stack(
+        children: [
+          Center(child: Image.asset(AppImages.union)),
+          Center(child: Image.asset(AppImages.logoFull))
+        ],
+      ),
+    );
+  }
+}
