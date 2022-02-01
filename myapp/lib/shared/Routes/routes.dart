00a31a93a9d4d263d579b58dfb8e4b8e38c23e80
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-02-01 13:53:47 -0300

    Adicionando homepage

--- myapp/lib/shared/Routes/routes.dart
+++ myapp/lib/shared/Routes/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/Pages/LoginPage.dart';
+import 'package:myapp/Pages/HomePage.dart';
+import 'package:myapp/Pages/login/LoginPage.dart';
 import 'package:myapp/Pages/SplashPage.dart';
 
 var routes = <String, WidgetBuilder>{
   '/splashPage': (context) => const SplashPage(),
-  '/loginPage': (context) => const LoginPage(),
+  '/loginPage': (context) => LoginPage(),
+  '/homePage': (context) => HomePage(),
 };
