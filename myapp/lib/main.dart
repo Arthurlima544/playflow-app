a62b46d32823b3e81f1950f6880499ed83df7e52
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-02-01 12:31:06 -0300

    Adicionando inicialização do firebase

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,24 +1,46 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:myapp/shared/consts/DisableBottonBar.dart';
 import 'package:myapp/shared/Routes/routes.dart';
 
+import 'shared/Components/error/ErrorFirebaseWidget.dart';
+import 'shared/Components/loading/LoadingWidget.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   disableBottomBarAndTopBar();
-  runApp(const MyApp());
+  runApp(const AppFirebase());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class AppFirebase extends StatefulWidget {
+  const AppFirebase({Key? key}) : super(key: key);
 
   @override
+  State<AppFirebase> createState() => _AppFirebaseState();
+}
+
+class _AppFirebaseState extends State<AppFirebase> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "My App",
-      routes: routes,
-      initialRoute: '/loginPage',
-      debugShowCheckedModeBanner: false,
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const ErrorFirebaseWidget();
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: "My App",
+            routes: routes,
+            initialRoute: '/loginPage',
+            debugShowCheckedModeBanner: false,
+          );
+        }
+        return const LoadingWidget();
+      },
     );
+    /*  */
   }
 }
