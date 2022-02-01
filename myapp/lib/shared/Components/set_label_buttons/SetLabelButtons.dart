b4ff3b5bbf3ca3eb4b92bdb57bc905e05e359935
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-02-01 19:41:04 -0300

    adicionando dupla de labelButton

---
+++ myapp/lib/shared/Components/set_label_buttons/SetLabelButtons.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/shared/Components/divider_vertical/DividerVerticalWidget.dart';
+import 'package:myapp/shared/Components/label_button/LabelButton.dart';
+
+class SetLabelButtons extends StatelessWidget {
+  final String primarylabel;
+  final VoidCallback primaryOnPressed;
+  final String secondarylabel;
+  final VoidCallback secondaryOnPressed;
+  const SetLabelButtons(
+      {Key? key,
+      required this.primarylabel,
+      required this.primaryOnPressed,
+      required this.secondarylabel,
+      required this.secondaryOnPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 56,
+      child: Row(
+        children: [
+          Expanded(
+            child: LabelButton(
+              label: primarylabel,
+              onPressed: primaryOnPressed,
+            ),
+          ),
+          DividerVerticalWidget(),
+          Expanded(
+            child: LabelButton(
+              label: secondarylabel,
+              onPressed: secondaryOnPressed,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
