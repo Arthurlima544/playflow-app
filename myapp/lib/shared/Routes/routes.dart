08690c4915ad3b4a4c6d39c9a1d3889317ba26b7
Arthurlima544 <arthurlindaolima544@gmail.com>
2022-02-01 19:39:04 -0300

    alterando rotas

--- myapp/lib/shared/Routes/routes.dart
+++ myapp/lib/shared/Routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/Pages/barcode/BarCodeScannerPage.dart';
 import 'package:myapp/Pages/home/HomePage.dart';
 import 'package:myapp/Pages/login/LoginPage.dart';
 import 'package:myapp/Pages/SplashPage.dart';
@@ -7,4 +8,5 @@ var routes = <String, WidgetBuilder>{
   '/splashPage': (context) => SplashPage(),
   '/loginPage': (context) => LoginPage(),
   '/homePage': (context) => HomePage(),
+  '/barCodePage': (context) => BarCodeScannerPage(),
 };
